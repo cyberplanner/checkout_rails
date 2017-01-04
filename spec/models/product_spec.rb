3b5d66d307601ffa9cd3d080425a6f1868ee0c40
cyberplanner <sal@salweb.co.uk>
2017-01-04 09:43:49 +0000

    More validations on products.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -14,5 +14,6 @@ describe Product, type: :model do
   it { should validate_presence_of :title }
   it { should validate_presence_of :price }
   it { should validate_numericality_of :price }
+  it { should validate_uniqueness_of :title }
 
 end
