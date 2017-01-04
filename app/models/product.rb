256abdfa4a5cf306d09d88472659dc82a752a790
cyberplanner <sal@salweb.co.uk>
2017-01-04 10:44:52 +0000

    Refactor product model specs.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
   validates :title, presence: true, uniqueness: true
+  validates_length_of :title, minimum: 5
   validates :price, presence: true, numericality: {greater_than_or_equal_to: 0.01}
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,6 +2,12 @@ require 'spec_helper'
 
 describe Product, type: :model do
 
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :price }
+  it { should validate_numericality_of :price }
+  it { should validate_uniqueness_of :title }
+  it { should validate_length_of(:title).is_at_least(5) }
+
   it "should have valid factory" do
     product = FactoryGirl.build(:product)
     expect(product).to be_valid
@@ -10,13 +16,15 @@ describe Product, type: :model do
   it "should require a title and a price" do
     product = FactoryGirl.build(:product, :title => "", :price => "")
     expect(product).not_to be_valid
-    expect(product).to have(1).error_on(:title)
+    expect(product).to have(2).error_on(:title)
     expect(product).to have(2).error_on(:price)
+    product.title = "xxxx"
+    expect(product).to have(1).error_on(:title)
+    product.title = "xxxxx"
+    product.price = 1.0
+    expect(product).to have(0).error_on(:price)
+    expect(product).to be_valid
   end
 
-  it { should validate_presence_of :title }
-  it { should validate_presence_of :price }
-  it { should validate_numericality_of :price }
-  it { should validate_uniqueness_of :title }
 
 end
