b7bd502caaa7c2a1aea06d27d7d4f66e5bbb4501
cyberplanner <sal@salweb.co.uk>
2017-01-04 09:37:29 +0000

    Validate price numericality

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,8 +6,13 @@ describe Product, type: :model do
     expect(FactoryGirl.build(:product)).to be_valid
   end
 
-  it "should require a username" do
+  it "should require a title and a price" do
     expect(FactoryGirl.build(:product, :title => "")).not_to be_valid
+    expect(FactoryGirl.build(:product, :price => "")).not_to be_valid
   end
+
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :price }
+  it { should validate_numericality_of :price }
 
 end
